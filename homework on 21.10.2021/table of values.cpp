8ab28ebab8e0c76ae017cc08a385ddcab4814415
MildenS <61003566+MildenS@users.noreply.github.com>
2021-10-20 22:52:47 +0400

    Add files via upload

---
+++ homework on 21.10.2021/table of values.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+double f(double a, double b, double x) {
+    return a * x * x * x + b / (x - 1);
+}
+
+int main()
+{
+    double a, b, c, d, h;
+    std::cin >> a >> b >> c >> d>>h;
+    std::cout << "a= " << a << "   b= " << b << std::endl;
+    for (double i = c; i <= d; i += h) {
+        std::cout << "x= " << i << "   f= " << f(a, b, i) << std::endl;
+    }
+
+}
+
