b71270a0cd970f4971822f1bcef02c86a5bfe799
MildenS <61003566+MildenS@users.noreply.github.com>
2021-10-20 23:21:38 +0400

    Add files via upload

---
+++ homework on 21.10.2021/number from 1 to n.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n;
+	std:: cin >> n;
+	if (n > 0) {
+		for (int i = 1; i <= n; ++i) {
+			std::cout << i << std:: endl;
+		}
+	}
+	else {
+		for (int i = n; i <= 0; ++i) {
+		std:: cout << i << std::endl;
+		}
+	}
+}
+
