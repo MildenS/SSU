a38e095c3bb3c145a0e5c0118fd9609702e9632a
MildenS <61003566+MildenS@users.noreply.github.com>
2021-10-20 22:27:05 +0400

    Add files via upload

---
+++ homework on 21.10.2021/second min plus from n.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    
+    int min = 100000;
+    int predmin = 100001;
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int cur, temp;
+        std::cin >>cur;
+        if (cur > 0) {
+            temp = cur;
+            if (cur < min) {
+                temp = min;
+                min = cur;
+            }
+            if (predmin > temp) predmin = temp;
+        }
+    }
+    std::cout << predmin;
+}
+
