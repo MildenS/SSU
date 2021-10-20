c98ab995db167d17462dd6d9c4ee9191ea014a38
MildenS <61003566+MildenS@users.noreply.github.com>
2021-10-20 22:16:28 +0400

    Add files via upload

---
+++ homework on 21.10.2021/3.summ of the negatives from m.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int m, s=0;
+    std::cin >> m;
+    for (int i = 0; i < m; ++i) {
+        int a;
+        std::cin >> a;
+        if (a < 0) s += a;
+    }
+    std::cout << s;
+}
+
