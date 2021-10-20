9796c88d752b6a740ecec7781cb4d9c1ca232c2a
MildenS <61003566+MildenS@users.noreply.github.com>
2021-10-20 23:32:29 +0400

    Add files via upload

---
+++ homework on 21.10.2021/continue.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+int main()
+{
+    int a;
+    std::vector <int> v;
+    do {
+        std::cin >> a;
+        if (a % 2 != 0) continue;
+        v.push_back(a);
+    } while (a != 0);
+    for (int i = 0; i < v.size(); ++i) {
+        std:: cout << v[i] << ' ';
+    }
+}
+
+
