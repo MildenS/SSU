a3fe51b67cbe3b9eace82e461ee7c8684ad90250
MildenS <61003566+MildenS@users.noreply.github.com>
2021-10-20 22:34:24 +0400

    Add files via upload

---
+++ homework on 21.10.2021/lot of odds.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int kolvo = 0;
+	int a;
+	std::cin >> a;
+	if (a != 0) {
+		do {
+			if (a % 2 != 0) kolvo++;
+			std::cin >> a;
+		} while (a != 0);
+	}
+	std::cout << kolvo;
+}
+
