00f307ccc24fc12bd68c5103bbe476c4d0957fda
MildenS <61003566+MildenS@users.noreply.github.com>
2021-11-18 00:44:44 +0400

    Update 1.cpp

--- homework on 18.11.2021/1.cpp
+++ homework on 18.11.2021/1.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <cmath>
 const double PI = 3.141592653589793238463;
+void valid(double r, double a);
 
 int main()
 {
-	double scircle, ssquare;
 	double r, a;
 	std::cin >> r >> a;
-	if (r > 0 && a > 0) {
-		scircle = PI * r * r;
-		ssquare = a * a;
+	valid(r, a);
+}
+void valid(double r, double a) {
+	if (r < 0 || a < 0) {
+		std::cout << "incorrect input";
+		return;
 	}
-	else {
-		std::cout << "incorrect input " << std::endl;
-		return 0;
-	}
+	double scircle = PI * r * r;
+	double ssquare = a * a;
 	if (scircle > ssquare) std::cout << "area of the circle is larger than square's area";
 	else if (scircle == ssquare) std::cout << "circle's area equal square's area";
 	else std::cout << "square's area larger than circle's area";
 }
-
