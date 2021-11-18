1c8360423ba85909c738fc52c4fb1a7992a0122e
MildenS <61003566+MildenS@users.noreply.github.com>
2021-11-18 09:36:48 +0400

    Update difference os sums.cpp

--- homework on 18.11.2021/difference os sums.cpp
+++ homework on 18.11.2021/difference os sums.cpp
@@ -8,13 +8,13 @@ int main()
 {
 	double sum = 0, sum1;
 	for (int i = 1; i <= n; i++) {
-		sum +=  1./i;
+		sum += 1. / i;
 	}
 	std::cout << std::fixed << std::setprecision(25) << sum << std::endl;
 	sum1 = sum;
 	sum = 0;
 	for (int i = n; i >= 1; i--) {
-		sum +=  1./i;
+		sum += 1. / i;
 	}
 	std::cout << std::fixed << std::setprecision(25) << sum << std::endl;
 	std::cout << "difference is " << abs(sum1 - sum) << std::endl;
