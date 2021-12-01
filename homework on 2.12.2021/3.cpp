9910c9fd225315ed7b874de9cc96781895494396
MildenS <61003566+MildenS@users.noreply.github.com>
2021-12-01 23:28:34 +0400

    Update 3.cpp

--- homework on 2.12.2021/3.cpp
+++ homework on 2.12.2021/3.cpp
@@ -1,1 +1,33 @@
+#include <iostream>  
 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << "Введите количество чисел ";
+	cin >> n;
+	int* arr = new int[n];
+	cout << "Введите числа\n";
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	cout << "\nBаша последовательность чисел до изменения:\n";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	if (n % 2 == 0) {
+		double temp = arr[n / 2];
+		arr[n / 2] = arr[n / 2 - 1];
+		arr[n / 2 - 1] = temp;
+	}
+	else {
+		arr[n / 2] *= 2;
+	}
+	cout << "\nВаша последовательность после изменения:\n";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i]<<" ";
+	}
+
+}
