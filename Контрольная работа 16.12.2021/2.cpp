d815f96b75428067f5ac08156f188cdb71758ac9
MildenS <61003566+MildenS@users.noreply.github.com>
2021-12-16 09:47:04 +0400

    Update 2.cpp

--- Контрольная работа 16.12.2021/2.cpp
+++ Контрольная работа 16.12.2021/2.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 void printArray(double* arr, int n);
 
+const double eps=1e-6;
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	cout << "Введите кол-во элементов: ";
 	int n;
-	int minnum=0, negnum=-1;
+	int minnum = 0, negnum = -1;
 	cin >> n;
 	if (n < 0) {
 		cout << "Отрицательного размера массива не бывает";
@@ -18,16 +20,16 @@ int main()
 	}
 	double* arr = new double[n];
 	cout << "Введите сами элементы:\n";
-	for(int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
-		if (arr[i] < 0)
+		if (arr[i]+eps < 0)
 			negnum = i;
-		if (arr[i] < arr[minnum])
+		if (arr[i]+eps < arr[minnum])
 			minnum = i;
 	}
 	cout << "Ваш массив до изменений:\n";
 	printArray(arr, n);
-	for (int i = minnum+1; i < negnum; i++) {
+	for (int i = minnum + 1; i < negnum; i++) {
 		arr[i] = 0;
 	}
 	cout << "Ваш массив после изменений:\n";
