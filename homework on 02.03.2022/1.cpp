55f9dd925ae92555528815234bbb4d8d9309763d
MildenS <61003566+MildenS@users.noreply.github.com>
2022-03-01 22:04:43 +0400

    Update 1.cpp

--- homework on 02.03.2022/1.cpp
+++ homework on 02.03.2022/1.cpp
@@ -1,1 +1,33 @@
+//эта задачка добавила мне седых волос
+#include <iostream>
+
+using namespace std;
+
+void drawLine(int x);
+void rec(int x, int n=1) {
+    drawLine(n);
+    if (x>n) {
+        rec(x, n + 1);
+        drawLine(n);
+    }
+}
+
+int main()
+{
+    rec(4);
+}
+
+void drawLine(int x)
+{
+    if (x % 2 != 0)
+        for (int i = 0; i < x; i++)
+            cout << '0';
+    else 
+        for (int i = 0; i < x; i++)
+            cout << '1';
+    cout << endl;
+}
+
+
+
 
