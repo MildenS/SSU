54aefd17acd6eac6932a1d79f35b423d37010d9c
MildenS <61003566+MildenS@users.noreply.github.com>
2022-03-29 22:59:29 +0400

    Create 4.cpp

---
+++ homework on 30.03.2022/4.cpp
@@ -0,0 +1,151 @@
+//4 сортировка BBall'ов 
+#include <iostream>
+
+using namespace std;
+
+class BBall
+{
+    int w;
+    string color;
+public:
+
+    static int n;
+
+    BBall()  // = default
+    {
+        w = 1;
+        color = "white";
+        n++;
+    }
+
+    //BBall(int w, string color) : w(w), color(color) {}
+    BBall(int w, string color)
+    {
+        setW(w);
+        setColor(color);
+    }
+
+    // BBall(const BBall & other) = delete;
+    BBall(const BBall& other)
+    {
+        w = other.w;
+        color = other.color;
+        n++;
+    }
+
+
+    bool setW(int w)
+    {
+        if (w > 0)
+        {
+            this->w = w;  // (*this).w = w;
+            return true;
+        }
+        return false;
+    }
+
+    bool setColor(string color)
+    {
+        if (color != "")
+        {
+            this->color = color;
+            return true;
+        }
+        return false;
+    }
+
+    int getW()
+    {
+        return w;
+    }
+
+    string getColor()
+    {
+        return color;
+    }
+
+    ~BBall()
+    {
+        n--;
+        //cout << "Deleted ball " << this->color << endl;
+    }
+
+
+    BBall& operator++()
+    {
+        w++;
+        return *this;
+    }
+
+    BBall operator++(int)
+    {
+        BBall tmp = *this;
+        w++;
+        return tmp;
+    }
+
+    friend BBall operator+(BBall& ball, int x)
+    {
+        return BBall(ball.w + x, ball.color);
+    }
+    friend BBall operator+(int x, BBall& ball)
+    {
+        return ball + x;
+    }
+    bool operator<(const BBall& ball);
+    bool operator==(const BBall& ball);
+    //вот он
+    BBall& operator=(const BBall& ball);
+    //
+};
+int BBall::n = 0;
+
+void BowlingSort(BBall* arr, int n);
+
+int main()
+{
+
+    int n, w;
+    string color;
+    cin >> n;
+    BBall* arr =new BBall[n];
+    for (int i = 0; i < n; i++) {
+        cin >> w >> color;
+        arr[i] = BBall(w, color);
+    }
+    cout << "\tYour BBalls before sort:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i].getW() << ' ' << arr[i].getColor() << endl;
+    }
+    BowlingSort(arr, n);
+    cout << "\tYour BBalls after sort:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i].getW() << ' ' << arr[i].getColor() << endl;
+    }
+    delete[] arr;
+    return 0;
+}
+
+bool BBall::operator<(const BBall& ball)
+{
+    return this->w < ball.w;
+}
+
+bool BBall::operator==(const BBall& ball)
+{
+    return (this->w == ball.w) && (this->color == ball.color);
+}
+
+BBall& BBall::operator=(const BBall& ball)
+{
+    this->color = ball.color;
+    this->w = ball.w;
+    return *this;
+}
+
+void BowlingSort(BBall* arr, int n)
+{
+    for (int i = 1; i < n; i++)
+        for (int j = i; j > 0 && (arr[j] < arr[j - 1]); j--)
+            swap(arr[j - 1], arr[j]);
+}
