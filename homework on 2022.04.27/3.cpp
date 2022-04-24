fce5cf72e20c1d8b9499205222b05c4e41f567c2
MildenS <61003566+MildenS@users.noreply.github.com>
2022-04-24 14:59:12 +0400

    Update 3.cpp

--- homework on 2022.04.27/3.cpp
+++ homework on 2022.04.27/3.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+void exerciseFunc(int n);
+
+int main() {
+	int n;
+	cin >> n;
+	exerciseFunc(n);
+}
+
+void exerciseFunc(int n)
+{
+	int temp;
+	queue<int> even;
+	stack<int> odd;
+	for(int i = 0; i < n; i++) {
+		cin >> temp;
+		if (abs(temp) % 2 == 0)
+			even.push(temp);
+		else
+			odd.push(temp);
+	}
+	cout << "------------------------\n";
+	while (!even.empty()) {
+		cout << even.front()<<' ';
+		even.pop();
+	}
+	while (!odd.empty()) {
+		cout << odd.top()<<' ';
+		odd.pop();
+	}
+}
 
