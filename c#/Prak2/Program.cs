89e46e463736aec3908c4fcc4de5b6ab1ed36aa1
MildenS <61003566+MildenS@users.noreply.github.com>
2022-09-07 22:21:15 +0400

    Add files via upload

---
+++ c#/Prak2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ какая из цифр трехзначного числа больше: первая или вторая; 
+*/
+
+namespace Pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a=Convert.ToInt32(Console.ReadLine());
+            string answer = (a > 999 || a < 100) ? "Incorrect input" :
+                (a / 100 > a / 10 % 10) ? "First digit is bigger then second" : (a / 100 == a / 10 % 10) ? "First and second digits is equal" :
+                "Second digit is bigger then first";
+            Console.WriteLine(answer);
+        }
+    }
+}
